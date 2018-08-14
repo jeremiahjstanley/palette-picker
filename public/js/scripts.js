25080620ed49ee42a6b66a11d09700eca08d1311
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-08-14 12:44:08 -0600

    Add color lock functionality to front end of the application

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,6 @@
-$('.palette').on('click', generatePalette)
-$('.lock-button').on('click', toggleLock)
+$(window).on('load', generatePalette);
+$('.palette').on('click', generatePalette);
+$('.lock-button').on('click', toggleLock);
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
@@ -8,21 +9,23 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
-}
+};
 
 function generatePalette() {
 	for (var i = 0; i < 5; i++) {
 		 var color = getRandomColor();
+		 if (!isLocked(i)) {
 	 		$(`.color-${[i]}-section`).css('background-color', color);
-	 		$(`.color-${[i]}-text`).text(`${color}`)	 
+	 		$(`.color-${[i]}-text`).text(`${color}`);	
+		 }
 	}
-}
+};
 
-function toggleLock() { 
+function toggleLock(event) { 
+	event.stopPropagation();
+	$(this).closest('section').toggleClass('locked');
+};
 
-}
-
-
-function isLocked() { 
-
-}
+function isLocked(i) { 
+	return $(`.color-${[i]}-section`).hasClass('locked')
+};
