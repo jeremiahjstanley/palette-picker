6b0ed43a87caed5152011f24e98ac427dc6a4835
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-08-20 17:06:36 -0600

    Correct postPalette method

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -36,7 +36,11 @@ function toggleLock(event) {
 };
 
 function isLocked(i) { 
-	return $(`.color-${[i]}-section`).hasClass('locked')
+	return $(`.color-${[i]}-section`).hasClass('locked');
+};
+
+function featurePalette() {
+
 };
 
 function savePalette(event) {
@@ -44,9 +48,13 @@ function savePalette(event) {
 	$('h3').each(function() {
 		colors.push($(this).text())
 	});
-	let input = $('.palette-input-field').val()
-	let project = 
-	colors = []
+	let paletteName = $('.palette-input-field').val();
+	let projectId = $('.project-dropdown').val();
+	if (paletteName.length && projectId) {
+		postPalette(projectId, paletteName, colors);
+		$('.palette-input-field').val('');
+	}
+	colors = [];
 };
 
 function saveProject(event) {
@@ -59,17 +67,11 @@ function saveProject(event) {
 	}
 };
 
-function populateDropdown(projects) {
-	projects.forEach(project => {
-		const { id, project_name } = project;
-		$('.project-dropdown').append(`<option value='${id}'>${project_name}</option>`);
-	});
-};
-
 async function getProjects() {
 	const url = '/api/v1/projects';
 	const response = await fetch(url);
 	const projects = await response.json()
+	getPalettes()
 	populateDropdown(projects)
 };
 
@@ -77,8 +79,21 @@ async function getPalettes() {
 	const url = '/api/v1/palettes';
 	const response = await fetch(url);
 	const palettes = await response.json()
-	
+	appendPalettes(palettes)
 };
+
+function populateDropdown(projects) {
+	projects.forEach(project => {
+		const { id, project_name } = project;
+		$('.project-dropdown').append(`<option value='${id}'>${project_name}</option>`);
+	});
+};
+
+function appendPalettes(palettes) {
+	palettes.forEach(palette => {
+
+	})
+}
 
 async function postProject(newProject) {
 	const url = '/api/v1/projects';
@@ -87,22 +102,34 @@ async function postProject(newProject) {
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: JSON.stringify(newProject)});
+		body: JSON.stringify(newProject)
+	});
 	const project = await response.json();
 	const id = project.id;
 	const name = newProject.project_name;
 	$('.project-dropdown').append(`<option value='${id}'>${name}</option>`);
-}
+};
 
-async	function postPalette(pallete) {
+async	function postPalette(id, name, colors) {
+	console.log(id, name, colors)
 	const url = '/api/v1/palettes';
+	const palette = {
+		palette_name: name,
+		color_1: colors[0],
+		color_2: colors[1],
+		color_3: colors[2],
+		color_4: colors[3],
+		color_5: colors[4],
+		project_id: id
+	};
 	const response = await fetch(url, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body:JSON.stringify({
+		body: JSON.stringify(palette)
+	});
+	const results = await response.json();
+};
 
-		})
-	})
-}
+
