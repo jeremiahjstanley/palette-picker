9431d6dd0f6e1b6075c3abab514c832b351402de
Jeremiah Stanley <jeremiah.j.stanley@gmail.com>
2018-08-21 16:38:58 -0600

    Fix typo in module.exports

--- db/knex.js
+++ db/knex.js
@@ -1,4 +1,4 @@
 const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile.js')[environment];
 
-module.export = require('knex')(configuration);
+module.exports = require('knex')(configuration);
